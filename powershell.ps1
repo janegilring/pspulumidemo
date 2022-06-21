b5cd6e2e6abd77e3cdd4010f7e09d461a97a5e64
Jan Egil Ring <jan.egil.ring@outlook.com>
2022-06-21 11:22:28 +0000

    Added storage account
    Signed-off-by: Jan Egil Ring <jan.egil.ring@outlook.com>

--- powershell.ps1
+++ powershell.ps1
@@ -4,4 +4,16 @@ New-PulumiYamlFile {
 
     $TestRG = New-AzureNativeResourcesResourceGroup -pulumiid testrg -resourceGroupName test-rg
 
+    $Props = @{
+        pulumiid          = "sa"
+        accountName       = "pspulumitest"
+        ResourceGroupName = $resourceGroup.reference("name")
+        location          = "norwayeast"
+        Kind              = "StorageV2"
+        Sku               = @{
+          Name = "Standard_LRS"
+        }
+      }
+      $storageAccount = azure_native_storage_storageaccount @Props
+
 }
